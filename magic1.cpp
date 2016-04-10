624f4d591b340ea6330b89a55d1b05ed5b0b0e7c
anmol verma <anmolvnit@gmail.com>
2016-04-10 13:41:20 +0530

    Added files via upload

---
+++ magic1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char a[10];
+	printf("********PRESS ENTER AFTER EACH COMMAND********\n");
+	printf("Think of a number.\n");
+	fgets(a, 100, stdin);
+	printf("Multiply it by 3.\n");
+	fgets(a, 100, stdin);
+	printf("Add 6 with the getting result.\n");
+	fgets(a, 100, stdin);
+	printf("divide it by 3.\n");
+	fgets(a, 100, stdin);
+	printf("Subtract it from the first number used.\n");
+	fgets(a, 100, stdin);
+	printf("AND THE NUMBER IS: 2\n");
+	return 0;
+}
